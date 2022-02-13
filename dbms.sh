063a5d9034f5f768b3ce4c35210769a9a6e589c4
DoAa-Atia18 <doaa.atia.ahmed@gmail.com>
2022-02-13 20:47:44 +0200

    Create and list tables

--- dbms.sh
+++ dbms.sh
@@ -3,7 +3,77 @@ export LC_COLLATE=C
 shopt -s extglob
 connectToDb=""
 returnFromFun=""
+#________________________Table Menu____________________________
 
+
+
+function TableMenu {
+echo "Please Select From Tabel Menu :)"
+PS3='You Select ==>'
+
+options=("CreateTable"
+            "List Tables"
+       "Drop Table"
+               "Insert Into Table"
+           "Select From Table"
+               "Delete From Table"
+           "Exit")
+select opt in "${options[@]}"
+do
+case $REPLY in
+                1) createTable ;;
+                2) listTables ;;
+                3) dropTable ;;
+                4) insertIntoTable ;;
+                5) selectFromTable ;;
+                6) deleteFromTable ;;
+                7) startDBMS ;;
+           *) echo invalid option $REPLY;;
+
+
+esac
+done
+}
+#________________________list Tables___________________________
+function listTables{
+  read -p "Please Enter DataBase Name: "  nameDatabase
+
+                if [[ $nameDatabase]; then
+                        echo "There are all tables ==> "
+                        ls ;
+                else
+                        echo "There is no DataBase with this Name :( "
+                        exit;
+                fi
+        TableMenu
+}
+
+
+#________________________start DBMS____________________________
+
+function startDBMS
+{
+PS3='Please select option: '
+options=("1- CREATE Database" "2- LIST ALL Database" "3- CONNECT TO Database" "4- DROP database" "5- EXIT")
+select opt in "${options[@]}"
+do
+	case $REPLY in 
+		1) createDatabase
+			;;
+		2) listDatabase
+			;;
+		3) connectToDatabase
+			;;
+		4) dropDatabase
+			;;
+		5) exit
+		       	;;
+		*) echo invalid option $REPLY;;
+	esac
+done
+}
+
+startDBMS
 #_______________________create database________________________
 
 function createDatabase
@@ -72,7 +142,7 @@ function connectToDatabase
 	listDatabaseToSelect
 	connectToDb=$returnFromFun
 	echo go to DB $connectToDb
-	########## func to list menu table
+	TableMenu 
 }
 
 #________________________drop database____________________________
@@ -85,29 +155,8 @@ function dropDatabase
         echo drop database $nameDbToDrop successfully 
 }
 
-#________________________creat database____________________________
 
-function startDBMS
-{
-PS3='Please select option: '
-options=("1- CREATE Database" "2- LIST ALL Database" "3- CONNECT TO Database" "4- DROP database" "5- EXIT")
-select opt in "${options[@]}"
-do
-	case $REPLY in 
-		1) createDatabase
-			;;
-		2) listDatabase
-			;;
-		3) connectToDatabase
-			;;
-		4) dropDatabase
-			;;
-		5) exit
-		       	;;
-		*) echo invalid option $REPLY;;
-	esac
-done
-}
 
-startDBMS
+
+
 
